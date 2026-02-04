21128445e71a62da58bedca31859a65a4a3ebe2e
radmilaradivojevic <radmila.radivojevic@trawa.de>
2026-02-04 18:29:03 +0100

    update config

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,2 +1,2 @@
-export const VITE_API_URL='http://localhost:8000/api'
-// export const VITE_API_URL='https://babyflex-api.onrender.com/api'
+// export const VITE_API_URL='http://localhost:8000/api'
+export const VITE_API_URL='https://babyflex-api.onrender.com/api'
